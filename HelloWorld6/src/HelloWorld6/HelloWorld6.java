9743adced234b0f6f080702063f5fd04fb855788
daniel.jimenez <daniel.jimenez@Aula15-Pc9.JUANXXIII.net>
2024-02-16 16:54:57 +0100

    commit1

---
+++ HelloWorld6/src/HelloWorld6/HelloWorld6.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package HelloWorld6;
+
+/**
+ * 
+ */
+public class HelloWorld6 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+		// TODO Auto-generated method stub
+
+	}
+
+}
